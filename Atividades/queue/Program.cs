ffd0d1feed277c5f6ea224812b63c96efd053ee7
Bianca Stefany Maccarini <353652@unoesc.lan>
2023-09-13 21:49:34 -0300

    aula 5

---
+++ Atividades/queue/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace QueueClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue fila = new Queue(); // Use a classe Queue do namespace StackClass
+
+            fila.Enqueue(10);
+            fila.Enqueue(9);
+            fila.Enqueue(100);
+            fila.Enqueue(50);
+
+            fila.PrintQueue();
+        }
+    }
+}
